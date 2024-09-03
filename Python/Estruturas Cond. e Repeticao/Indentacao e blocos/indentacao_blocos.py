ee8ee44128aafb77808dbc77276e88be48d9a32f
fabiovdasilva <fabioviniciusdasilva@outlook.com>
2024-09-03 10:06:14 -0300

    first commit

---
+++ Python/Estruturas Cond. e Repeticao/Indentacao e blocos/indentacao_blocos.py
@@ -0,0 +1,8 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("Valor sacado!")
+
+#4 espacos para estar dentro do bloco
+sacar(100)
---
+++ Python/Operadores/Associacao/operadores_associacao.py
@@ -0,0 +1,9 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limao"]
+saques = [1500, 100]
+
+print("Python" in curso)
+
+print("maca" not in frutas)
+
+print(200 in saques)
