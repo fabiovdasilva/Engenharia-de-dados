ca1c9e2e9b91efb5bd0133cf725a66a13fba51b1
fabiovdasilva <fabioviniciusdasilva@outlook.com>
2024-09-04 10:00:12 -0300

    remembering

--- Python/Estruturas Cond. e Repeticao/Estruturas condicionais/estruturas_condicionais.py
+++ Python/Estruturas Cond. e Repeticao/Estruturas condicionais/estruturas_condicionais.py
@@ -6,3 +6,14 @@ if saldo >= saque:
 
 if saldo < saque:
     print("Saldo insuficiente!")
+
+opcao = int(input("Informe uma opcao:   [1] Sacar \n[2] Extrato: "))
+
+if opcao == 1:
+    valor = float(input("informe a quantia para o saque: "))
+
+elif opcao == 2:
+    print("Exibindo extrato...")
+else:
+    print("Opcao Invalida")
+
---
+++ Python/Estruturas Cond. e Repeticao/Estruturas condicionais/if_ternario.py
@@ -0,0 +1,1 @@
+status = "Sucesso" if saldo >= saque else "Falha"
